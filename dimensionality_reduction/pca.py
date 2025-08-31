d393923d9044254c07e337420b7a323ebb6d97d0
evgenyorlov1 <evgenyorlov1@gmail.com>
2025-08-31 22:07:48 +0300

    fix: remove unused imports

--- dimensionality_reduction/pca.py
+++ dimensionality_reduction/pca.py
@@ -1,11 +1,9 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
-from mpl_toolkits.mplot3d import Axes3D
 
 
 # load data
